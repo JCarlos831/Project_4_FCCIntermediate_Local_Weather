2f2255b0d65a0cf741b701436eaa9302687ec9ce
Juan Montoya <jcmontoya@gmail.com>
2017-12-04 20:07:50 +0000

    longitude and latitude now appearing

--- weather.js
+++ weather.js
@@ -5,34 +5,34 @@
 */
 
 $(document).ready(function(){
-    
+     var long;
+     var lat;
+
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-            $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
-            
+             lat = position.coords.latitude;
+             long = position.coords.longitude;
+            $("#weatherDetails").html("latitude: " + lat + "<br>longitude: " + long);
+
         });
-        
+
     }
-    
-    var api = "http://api.openweathermap.org/data/2.5/weather?id=2172797&appid=" + APIKEY;
-    
+    var api = "http://api.openweathermap.org/data/2.5/weather?id=2172797&appid=43554e9551b1521266276edbd5d60132";
+
+
+    $.getJSON(api, function(data){
+ });
+
     var header = document.createElement("H1");
         header.setAttribute("id", "title");
-        header.setAttribute("style", "color: white");
+        header.setAttribute("style", "color: whtie");
         header.innerHTML = "Local Weather App";
     var sun = document.createElement("I");
         sun.setAttribute("class", "fa fa-sun-o");
         sun.setAttribute("aria-hidden", "true");
     document.getElementById("header").appendChild(header);
-    document.getElementById("header").appendChild(sun);  
-    
-    $.getJSON(api, function(data){
-        alert(data.coord.lon);
-    });
-    
-    
-    
+    document.getElementById("header").appendChild(sun);
+
+
+
 });
-
-
-
