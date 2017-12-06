8deb854f741eed756734053663b6b067f7eab391
Juan Montoya <jcmontoya@gmail.com>
2017-12-06 01:36:47 +0000

    Temp showing in F, icon showing, location showing

--- weather.js
+++ weather.js
@@ -1,17 +1,27 @@
-/*global $*/
+/*global $
+  global APIKEY
+*/
 
 
 $(function(){
 	
 	$.getJSON('//freegeoip.net/json/').done(function(location){
 		$('#country').html(location.country_name);
-		$('#city').html(location.city +', '+location.region_code);
+		$('#location').html(location.city +', '+location.region_code+', '+location.country_name);
 		
 		var lat = location.latitude;
 		var long = location.longitude;
 		
-		$.getJSON('//api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long, function(data){
+		$.getJSON('//api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+APIKEY+'&units=imperial', function(data){
 			console.log(data);
+			
+			var temp = Math.round(data.main.temp)+'&#8457';
+			var weatherType = data.weather[0].description;
+			var weatherIcon = '<img src='+'http://openweathermap.org/img/w/'+data.weather[0].icon+'.png'+'>';
+			
+			$('#temp').html(temp);
+			$('#temp').prepend(weatherIcon);
+			$('#weatherType').html(weatherType);
 		});
 		
 		
