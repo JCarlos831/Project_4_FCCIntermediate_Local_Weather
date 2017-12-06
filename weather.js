14da52013b646940045257e9fe19772601277164
Juan Montoya <jcmontoya@gmail.com>
2017-12-06 19:28:15 +0000

    Seems to be working, except for F/C toggle

--- weather.js
+++ weather.js
@@ -4,6 +4,8 @@
 
 
 $(function(){
+  
+  
 	
 	$.getJSON('//freegeoip.net/json/').done(function(location){
 		$('#country').html(location.country_name);
@@ -15,28 +17,23 @@ $(function(){
 		$.getJSON('//api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+APIKEY+'&units=imperial', function(data){
 			console.log(data);
 			
-			var temp = Math.round(data.main.temp)+'&#8457';
+			var fTemp = Math.round(data.main.temp)+'&#8457';  //fahrenheit temperature
+			var cTemp = (Math.round((Math.round(data.main.temp)-32) * (5/9)))+'&#8451;';  //celsius temperature
+		// 	console.log(cTemp);
+		// 	console.log(fTemp);
 			var weatherType = data.weather[0].description;
-			var weatherIcon = '<img src='+'http://openweathermap.org/img/w/'+data.weather[0].icon+'.png'+'>';
+			var weatherIcon = '<img src='+'http://openweathermap.org/img/w/'+data.weather[0].icon+'.png'+'>';  
 			
-			$('#temp').html(temp);
+			$('#temp').html(fTemp);
 			$('#temp').prepend(weatherIcon);
+
+			$('#tempSwitch').click(function(){
+			  
+			  
+			});
+			
+			
 			$('#weatherType').html(weatherType);
-		});
-		
-		
-		
-		
-		
-		
-		
-	});
-	
-	
-	
-	
-	
-	
-	
-	
-})
+		});  //openweathermap.org
+	});  //freegeoip.net
+});  //ready function
