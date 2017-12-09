a8ac76e2dac2d7272830808dc18dc95b43f198a6
Juan Montoya <jcmontoya@gmail.com>
2017-12-09 00:35:02 +0000

    added weather icon

--- weather.js
+++ weather.js
@@ -1,32 +1,27 @@
-/*global $*/
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+  	
+  	var lat = position.coords.latitude;
+  	var long = position.coords.longitude;
+
+  	$.getJSON('//api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&units=imperial'+APIKEY).done(function(data){
+  		console.log(data);
+  		
+  		var fTemp = Math.round(data.main.temp)+ '&#8457;';
+  		var cTemp = Math.round((data.main.temp -32)* (5/9)+ '&#8451;');
+
+  		$('#weatherIcon').html('<img src='+'http://openweathermap.org/img/w/'+data.weather[0].icon+'.png>');
+  		$('#temp').html(fTemp);
+  		$('#weatherType').html(data.weather[0].description);
+  		$('#location').html(data.name);
+  		console.log(data.weather[0].icon);
+  		
+  	});
 
 
-$(function(){
-	
-	$.getJSON('//freegeoip.net/json/').done(function(location){
-		$('#country').html(location.country_name);
-		$('#city').html(location.city +', '+location.region_code);
-		
-		var lat = location.latitude;
-		var long = location.longitude;
-		
-		$.getJSON('//api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long, function(data){
-			console.log(data);
-		});
-		
-		
-		
-		
-		
-		
-		
-	});
-	
-	
-	
-	
-	
-	
-	
-	
-})
+
+
+
+
+  });
+}
