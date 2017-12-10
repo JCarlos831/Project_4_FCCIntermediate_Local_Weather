e0ab7a78b8a4ea5f6fba1010cec346e3f6a3d2fc
Juan Montoya <jcmontoya@gmail.com>
2017-12-10 04:27:49 +0000

    It's working...

--- weather.js
+++ weather.js
@@ -1,23 +1,48 @@
 /*global $
   global APIKEY
 */
+
 $(function(){
 
-	if (navigator.geolocation) {
-	  navigator.geolocation.getCurrentPosition(function(position) {
-			var lat = position.coords.latitude;
-			var long = position.coords.longitude;
-			$.getJSON('//api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&units=imperial' + APIKEY).done(function(data) {
-				var fTemp = Math.round(data.main.temp) + '&#8457;';
-				var cTemp = Math.round((data.main.temp - 32) * (5 / 9) + '&#8451;');
-				$('#location').html('Current Weather in '+'<br>'+data.name);
-				$('#weatherIcon').html('<img src=' + 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png>');
-				$('#temp').html(fTemp);
-				$('#weatherDesc').html(data.weather[0].description);
-				$('#weatherType').html(data.weather[0].main);
-				
-			
-			});
-		});
+	var C = false;
+
+		function displayTemp(F,C){
+			if(C)
+			return Math.round((F-32)*(5/9))+'&deg; C';
+			return Math.round(F)+'&deg; F';
+		}
+
+		function displayWeather(data,C){
+			var weatherDesc = data.weather[0].description;
+			var temp = displayTemp(data.main.temp,C);
+			var icon = data.weather[0].icon;
+			var weatherType = data.weather[0].main;
+
+			$('#temp').html(temp);
+			$('#weatherDesc').html(weatherDesc);
+			$('#weatherType').html(weatherType);
+
+			var apiIcon = 'http://openweathermap.org/img/w/' + icon + '.png';
+			$('#weatherIcon').html('<img src='+apiIcon+'>');
+		}
+		
+			if (navigator.geolocation) {
+				navigator.geolocation.getCurrentPosition(function(position) {
+					var lat = position.coords.latitude;
+					var long = position.coords.longitude;
+
+			$.getJSON('https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&units=imperial'+APIKEY, function(data){
+				$('#location').html('Current weather in ' + '<br>' +data.name+ '<br>'+ 'is')
+				$('#windSpeed').html('The current wind speed is ' + Math.round(data.wind.speed) + ' mph');
+
+				displayWeather(data,C)
+
+
+				$('#switch').click(function(){
+					C=!C;
+					displayWeather(data,C);
+				})
+			})
+		})
 	}
-});
+})
